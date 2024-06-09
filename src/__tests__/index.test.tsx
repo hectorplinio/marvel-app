857c3f5f929d5d396de09c42b28c15ff05986bad
Héctor Plinio Navarro Navarro <73659203+hectorplinio@users.noreply.github.com>
2024-06-09 18:25:11 +0200

    feat: deploy to vercel

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Home from "../index";
 import { FavoritesProvider } from "@contexts/FavoritesContext";
+import Home from "../pages/index";
 
 describe("Home", () => {
   it("renders the heading", () => {
     render(
       <FavoritesProvider>
         <Home />
-      </FavoritesProvider>,
+      </FavoritesProvider>
     );
   });
 });
