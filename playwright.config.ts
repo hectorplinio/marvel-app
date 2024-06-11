a75779cf860419da2aae195dc1c4c5d5e0acf4d4
Héctor Plinio Navarro Navarro <73659203+hectorplinio@users.noreply.github.com>
2024-06-11 12:05:24 +0200

    feat: add playwright docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   reporter: "html",
   use: {
     actionTimeout: 0,
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
     trace: "on-first-retry",
   },
   projects: [
